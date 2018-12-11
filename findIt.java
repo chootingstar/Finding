e4ea702ea1809d2e43fa17ae567a6a852073a0e4
chootingstar <jmirayes96@gmail.com>
2018-12-11 11:30:35 -0800

    update one

--- findIt.java
+++ findIt.java
@@ -2,6 +2,10 @@
 public class findIt {
 	public static void main(String args[]) {
 		System.out.println("This class supposed find stuff on the internet");
+		int a = 16;
+		int b = 4;
+		int c = 8;
+		System.out.println(-b * a <= a + c);
 	}
 	
 //This should work
